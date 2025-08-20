ad7606c58438d4254ef1ff8881d0e8416bd0ddef
zhequan00 <zhequan05@gmail.com>
2025-08-20 15:23:00 +0800

    Level-3: Use Task class; add mark/unmark functionality

--- src/main/java/Yang.java
+++ src/main/java/Yang.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Yang {
     public static void main(String[] args) {
@@ -8,11 +9,10 @@ public class Yang {
         System.out.println("___________________________________________________");
 
         Scanner sc = new Scanner(System.in);
-        String[] tasks = new String[100];
-        int taskCount = 0;
+        ArrayList<Task> tasks = new ArrayList<>();
 
         while (true) {
-            String input = sc.nextLine();
+            String input = sc.nextLine().trim();
 
             if (input.equals("bye")) {
                 System.out.println("_______________________________________");
@@ -21,13 +21,29 @@ public class Yang {
                 break;
             } else if (input.equals("list")) {
                 System.out.println("_______________________________________");
-                for (int i = 0; i < taskCount; i++) {
-                    System.out.println((i+1) + ". " + tasks[i]);
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.println((i + 1) + ". " + tasks.get(i));
                 }
                 System.out.println("_______________________________________");
+            } else if (input.startsWith("mark")) {
+                int idx = Integer.parseInt(input.substring(5)) - 1;
+                Task t = tasks.get(idx);
+                t.markAsDone();
+                System.out.println("_______________________________________");
+                System.out.println("Perfect! I've marked this task as done");
+                System.out.println(" " + t);
+                System.out.println("_______________________________________");
+            } else if (input.startsWith("unmark")) {
+                int idx = Integer.parseInt(input.substring(7)) - 1;
+                Task t = tasks.get(idx);
+                t.markAsUndone();
+                System.out.println("_______________________________________");
+                System.out.println("Alright, I've marked this task as not done yet");
+                System.out.println(" " + t);
+                System.out.println("_______________________________________");
             } else {
-                tasks[taskCount] = input;
-                taskCount++;
+                Task t = new Task(input);
+                tasks.add(t);
                 System.out.println("_______________________________________");
                 System.out.println("added: " + input);
                 System.out.println("_______________________________________");
