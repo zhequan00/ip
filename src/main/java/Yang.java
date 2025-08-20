208f71e1df369f267cca4928ae35a496d1ea4187
zhequan00 <zhequan05@gmail.com>
2025-08-20 12:09:57 +0800

    Level-0: Rename bot and add greeting + exit

---
+++ src/main/java/Yang.java
@@ -0,0 +1,12 @@
+public class Yang {
+    public static void main(String[] args) {
+        System.out.println("___________________________");
+        System.out.println("Hello! I'm Yang");
+        System.out.println("What can I do for you?");
+        System.out.println("___________________________");
+
+        System.out.println("Bye Bye! Hope to see you again soon!");
+        System.out.println("___________________________");
+
+    }
+}
