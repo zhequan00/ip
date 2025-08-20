3ed629c9c1bd1c03fbf2d4719ecafd5c45e7db4a
zhequan00 <zhequan05@gmail.com>
2025-08-20 12:33:43 +0800

    Level-1: Implement echo functionality

--- src/main/java/Yang.java
+++ src/main/java/Yang.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Yang {
     public static void main(String[] args) {
         System.out.println("___________________________");
@@ -5,8 +7,24 @@ public class Yang {
         System.out.println("What can I do for you?");
         System.out.println("___________________________");
 
-        System.out.println("Bye Bye! Hope to see you again soon!");
-        System.out.println("___________________________");
+        Scanner sc = new Scanner(System.in);
+        String input;
+
+        while (true) {
+            input = sc.nextLine();
+            if (input.equals("bye")) {
+                System.out.println("_______________________________________");
+                System.out.println("Bye bye! Hope to see you again soon!");
+                System.out.println("_______________________________________");
+                break;
+            } else {
+                System.out.println("_______________________________________");
+                System.out.println(input);
+                System.out.println("_______________________________________");
+            }
+        }
+
+        sc.close();
 
     }
 }
