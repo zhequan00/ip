01f8ca96afe48f1fc1f227f26a887c9e8ccadb83
zhequan00 <zhequan05@gmail.com>
2025-08-27 15:16:19 +0800

    A-JUnit: add Parser and TaskList tests

---
+++ src/test/java/yang/task/TaskListTest.java
@@ -0,0 +1,33 @@
+package yang.task;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TaskListTest {
+
+    @Test
+    void markAndUnmark_toggleDoneFlag() {
+        TaskList tl = new TaskList();
+        Todo t = new Todo("read book");
+        tl.add(t);
+
+        assertFalse(t.isDone());
+        tl.mark(0);
+        assertTrue(t.isDone());
+        tl.unmark(0);
+        assertFalse(t.isDone());
+    }
+
+    @Test
+    void addAndRemove_updatesSizeAndReturnsRemovedTask() {
+        TaskList tl = new TaskList();
+        tl.add(new Todo("a"));
+        tl.add(new Todo("b"));
+        assertEquals(2, tl.size());
+
+        Task removed = tl.remove(0);
+        assertEquals(1, tl.size());
+        assertTrue(removed.toString().contains("a")); // sanity on removed item
+    }
+}
