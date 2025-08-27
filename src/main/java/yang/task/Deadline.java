2387577f0949d37c2a5deb779576db7a2f48b2fa
zhequan00 <zhequan05@gmail.com>
2025-08-27 12:56:31 +0800

    Level-8: Add date and time functionality

--- src/main/java/yang/task/Deadline.java
+++ src/main/java/yang/task/Deadline.java
@@ -1,12 +1,29 @@
-// yang/task/Deadline.java
 package yang.task;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public final class Deadline extends Task {
-    private final String by;
+    private final LocalDate by;
+    private static final DateTimeFormatter OUT = DateTimeFormatter.ofPattern("MMM d yyyy");
 
-    public Deadline(String description, String by) { super(description, false); this.by = by; }
-    public Deadline(String description, String by, boolean isDone) { super(description, isDone); this.by = by; }
+    public Deadline(String description, LocalDate by) {
+        super(description, false);
+        this.by = by;
+    }
 
-    @Override public String toString() { return "[D][" + (isDone ? "X" : " ") + "] " + description + " (by: " + by + ")"; }
-    @Override public String toStorage() { return "D | " + (isDone ? "1" : "0") + " | " + description + " | " + by; }
+    public Deadline(String description, LocalDate by, boolean isDone) {
+        super(description, isDone);
+        this.by = by;
+    }
+
+    @Override
+    public String toString() {
+        return "[D][" + (isDone ? "X" : " ") + "] " + description + " (by: " + by.format(OUT) + ")";
+    }
+
+    @Override
+    public String toStorage() {
+        return "D | " + (isDone ? "1" : "0") + " | " + description + " | " + by;
+    }
 }
--- src/main/java/yang/task/Task.java
+++ src/main/java/yang/task/Task.java
@@ -9,9 +9,15 @@ public abstract class Task {
         this.isDone = isDone;
     }
 
-    public void markDone()   { this.isDone = true; }
-    public void markUndone() { this.isDone = false; }
-    public boolean isDone()  { return isDone; }
+    public void markDone() {
+        this.isDone = true;
+    }
+    public void markUndone() {
+        this.isDone = false;
+    }
+    public boolean isDone() {
+        return isDone;
+    }
 
     public abstract String toStorage();
 
@@ -20,10 +26,11 @@ public abstract class Task {
         if (p.length < 3) return null;
         boolean done = "1".equals(p[1]);
         String desc = p[2];
+
         switch (p[0]) {
             case "T": return new Todo(desc, done);
-            case "D": return (p.length >= 4) ? new Deadline(desc, p[3], done) : null;
-            case "E": return (p.length >= 4) ? new Event(desc, p[3], done) : null;
+            case "D": return (p.length >= 4) ? new Deadline(desc, java.time.LocalDate.parse(p[3]), done) : null;
+            case "E": return (p.length >= 4) ? new Event(desc, java.time.LocalDate.parse(p[3]), done) : null;
             default:  return null;
         }
     }
