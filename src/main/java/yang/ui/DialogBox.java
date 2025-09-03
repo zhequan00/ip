17ff4ba3ba970d1cb42a557d71618bcccf97c97a
zhequan00 <zhequan05@gmail.com>
2025-09-04 00:36:20 +0800

    Add basic JavaFX GUI with styled controls

---
+++ src/main/java/yang/ui/DialogBox.java
@@ -0,0 +1,25 @@
+package yang.ui;
+
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+
+public class DialogBox extends HBox {
+
+    private final Label text;
+    private final ImageView displayPicture;
+
+    public DialogBox(String s, Image i) {
+        text = new Label(s);
+        displayPicture = new ImageView(i);
+
+        text.setWrapText(true);
+        displayPicture.setFitWidth(100.0);
+        displayPicture.setFitHeight(100.0);
+        this.setAlignment(Pos.TOP_RIGHT);
+
+        this.getChildren().addAll(text, displayPicture);
+    }
+}
